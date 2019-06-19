b3efbc7218fafd5dab0e813f7f491829fb995a56
masato.kanamori <masato.kanamori@rakus.co.jp>
2019-06-19 17:48:12 +0900

    SpringSecurityで強制的にログインを求められる機能の一時的解除

--- src/main/java/com/example/ecommerce_c/config/WebSecurityConfig.java
+++ src/main/java/com/example/ecommerce_c/config/WebSecurityConfig.java
@@ -1,5 +1,26 @@
 package com.example.ecommerce_c.config;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration // 設定用のクラス
+@EnableWebSecurity // Spring Securityのウェブ用の機能を利用する
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/css/**", "/img/**", "/js/**");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests() // 認可に関する設定
+		.antMatchers("/**").permitAll() //「/」などのパスは全てのユーザに許可
+		.anyRequest().authenticated(); // それ以外のパスは認証が必要
+	}
+	
+	
 
 }
