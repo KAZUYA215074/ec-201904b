ffda46b86225232f362c4679c0b0e7fe02d18f17
ryuhei.sugita <ryuhei.sugita@rakus.co.jp>
2019-06-19 17:57:06 +0900

    ドメインの作成（Item,Topping,OrderTopping）

---
+++ src/main/java/com/example/ecommerce_c/domain/Topping.java
@@ -0,0 +1,57 @@
+package com.example.ecommerce_c.domain;
+
+public class Topping {
+	
+	/** ID */
+	private Integer id;
+	/** 名前 */
+	private String name;
+	/** Mの価格 */
+	private String priceM;
+	/** Lの価格 */
+	private String priceL;
+
+	@Override
+	public String toString() {
+		return "Topping [id=" + id + ", name=" + name + ", priceM=" + priceM + ", priceL=" + priceL + "]";
+	}
+	
+	/** 引数なしのコンストラクタ */
+	public Topping() {}
+	
+	/** コンストラクタ */
+	public Topping(Integer id, String name, String priceM, String priceL) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.priceM = priceM;
+		this.priceL = priceL;
+	}
+
+     /** getter/setter */
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPriceM() {
+		return priceM;
+	}
+	public void setPriceM(String priceM) {
+		this.priceM = priceM;
+	}
+	public String getPriceL() {
+		return priceL;
+	}
+	public void setPriceL(String priceL) {
+		this.priceL = priceL;
+	}
+
+}
