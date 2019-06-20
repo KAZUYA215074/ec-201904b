89f308aed4c754146cb9927e153093de29e379c4
momoyo.kanie <momoyo.kanie@rakus.co.jp>
2019-06-20 15:05:22 +0900

    商品一覧を表示する機能を作成しました。 表示形式をまだ整えていないです。（表示をするだけ）

--- src/main/java/com/example/ecommerce_b/controller/ShowItemListController.java
+++ src/main/java/com/example/ecommerce_b/controller/ShowItemListController.java
@@ -1,7 +1,14 @@
 package com.example.ecommerce_b.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.ecommerce_b.domain.Item;
+import com.example.ecommerce_b.service.GetItemListService;
 
 /**
  * 商品一覧の表示・検索を行うコントローラ.
@@ -11,5 +18,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class ShowItemListController {
+
+	@Autowired
+	private GetItemListService getItemListService;
+
+	/**
+	 * 商品一覧を表示する.
+	 * 
+	 * @param status 並び替えるパラメータ
+	 * @param model  モデル
+	 * @return 商品一覧表示画面
+	 */
+	@RequestMapping("")
+	public String showList(String status, Model model) {
+		
+		System.out.println("/開始");
+		if(status == null) {
+			status = "id";
+		}
+		List<Item> itemList = getItemListService.getAll(status);
+		System.out.println(itemList.size());
+		model.addAttribute("itemList", itemList);
+
+		return "item_list";
+	}
 
 }
--- src/main/java/com/example/ecommerce_b/service/GetItemListService.java
+++ src/main/java/com/example/ecommerce_b/service/GetItemListService.java
@@ -1,7 +1,14 @@
 package com.example.ecommerce_b.service;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.example.ecommerce_b.domain.Item;
+import com.example.ecommerce_b.repository.ItemRepository;
 
 /**
  * 商品一覧の表示・検索を行うサービス.
@@ -11,5 +18,50 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class GetItemListService {
+
+	@Autowired
+	private ItemRepository itemRepository;
+
+	/**
+	 * 商品一覧を取得する.<br>
+	 * statusのパラメータで並び替えをする。
+	 * 
+	 * @param status 並び替えをするパラメータ
+	 * @return 取得した商品情報一覧
+	 */
+	public List<Item> getAll(String status) {
+		List<Item> itemList = itemRepository.findAll(status);
+
+		return sortByStatus(status, itemList);
+	}
+
+	/**
+	 * 名前のあいまい検索を行う.<br>
+	 * statusのパラメータで並び替えをする。
+	 * 
+	 * @param name   検索を行う文字列
+	 * @param status 並び替えをするパラメータ
+	 * @return 取得した商品情報一覧
+	 */
+	public List<Item> searchLikeName(String name, String status) {
+		List<Item> itemList = itemRepository.findLikeName(name, status);
+
+		return sortByStatus(status, itemList);
+	}
+
+	/**
+	 * 与えられたパラメータで並び替えをする. ステータスが"高い"なら並び替えを行う.
+	 * 
+	 * @param status
+	 * @return 並び替えをした商品情報一覧
+	 */
+	public List<Item> sortByStatus(String status, List<Item> itemList) {
+
+		if ("高い".equals(status)) {
+			Collections.reverse(itemList);
+		}
+
+		return itemList;
+	}
 
 }
