a4a6861f5bd92cc6567a6b939726392c154c684a
masato.kanamori <masato.kanamori@rakus.co.jp>
2019-06-20 15:45:44 +0900

    ユーザ登録機能の実装

--- documents/sql/showAll.sql
+++ documents/sql/showAll.sql
@@ -1,6 +1,8 @@
+--nsert into users(name, email, password, zipcode, address, telephone) values('テストユーザ', 'test3@test.co.jp', '$2a$08$8aflfwUJQeMv4PbHfib6tOko6sHg9IXixhNVbMlUJoaUx1lRbZA5K','1111111', 'テスト住所', 'テスト電話番号');
 SELECT id,name, email, password, zipcode, address, telephone FROM users;
 SELECT id,name,description , price_m , price_l , image_path , deleted FROM items;
 SELECT id,name,price_m,price_l FROM toppings;
 SELECT id ,user_id , status , total_price , order_date , destination_name , destination_email , destination_zipcode , destination_address,destination_tel , delivery_time , payment_method FROM orders;
 SELECT id , item_id , order_id , quantity , size FROM order_items;
 SELECT id , topping_id , order_item_id FROM order_toppings ;
+select id,name, email, password, zipcode, address, telephone from users where email='test2@test.co.jp'
--- src/main/java/com/example/ecommerce_b/config/WebSecurityConfig.java
+++ src/main/java/com/example/ecommerce_b/config/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.ecommerce_b.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration // 設定用のクラス
 @EnableWebSecurity // Spring Securityのウェブ用の機能を利用する
@@ -21,6 +24,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.anyRequest().authenticated(); // それ以外のパスは認証が必要
 	}
 	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+    		return new BCryptPasswordEncoder();
+    }
 	
 
 }
