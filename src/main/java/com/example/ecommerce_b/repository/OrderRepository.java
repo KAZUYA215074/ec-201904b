fd852ab93f5956eb2eb1f068923fd7b36216ba92
ryuhei.sugita <ryuhei.sugita@rakus.co.jp>
2019-06-20 14:12:42 +0900

    orderRepository作成途中

--- src/main/java/com/example/ecommerce_b/repository/OrderRepository.java
+++ src/main/java/com/example/ecommerce_b/repository/OrderRepository.java
@@ -1,10 +1,14 @@
 package com.example.ecommerce_b.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.ecommerce_b.domain.Order;
+import com.example.ecommerce_b.form.OderForm;
 
 /**
  * odersテーブルを操作するリポジトリ.
@@ -25,7 +29,9 @@ public class OrderRepository {
 	 * @return　指定したユーザーIDと状態に一致する注文情報
 	 */
 	public Order findByUserIdAndStatus(int userId,int status) {
-		
+		String sql = "";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("userId", userId).addValue("status", status);
+//		Order order = template.
 		return null;
 	}
 }
