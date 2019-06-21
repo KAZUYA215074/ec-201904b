c6c1be1e8621c9f00009e0272f59fb84d32caf19
ayane.tanaka <ayane.tanaka@rakus.co.jp>
2019-06-21 13:18:23 +0900

    ショッピングカートの中身表示完成

--- src/main/java/com/example/ecommerce_b/controller/CartController.java
+++ src/main/java/com/example/ecommerce_b/controller/CartController.java
@@ -1,7 +1,10 @@
 package com.example.ecommerce_b.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.ecommerce_b.domain.Order;
@@ -14,12 +17,26 @@ public class CartController {
 		@Autowired
 		private CartService cartService;
 		
+		@Autowired
+		private HttpSession session;
+
 		
 		@RequestMapping("/show-cart")
-		public String showCart() {
-			
-//			Order cart=cartService.loadOrder(1);
-//			System.out.println(cart);
+		public String showCart(Model model) {
+			session.setAttribute("userId", 1);
+			Integer userId=(Integer)session.getAttribute("userId");
+			if(userId==null) {
+				model.addAttribute("cartStatus",false);
+				return "cart_list";
+			}
+			Order cart=cartService.loadOrder(userId);
+			if(cart==null) {
+				model.addAttribute("cartStatus",false);
+				return "cart_list";
+			}
+			System.out.println(cart);
+			model.addAttribute("cartStatus", true);
+			model.addAttribute("cart", cart);
 			return "cart_list";
 		}
 }
