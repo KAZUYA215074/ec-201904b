8bf16ca85e74d0bef1ee0556150c273534f1a00d
momoyo.kanie <momoyo.kanie@rakus.co.jp>
2019-06-21 15:45:15 +0900

    商品のあいまい検索機能を追加しました。

--- src/main/java/com/example/ecommerce_b/controller/ShowItemListController.java
+++ src/main/java/com/example/ecommerce_b/controller/ShowItemListController.java
@@ -23,8 +23,7 @@ public class ShowItemListController {
 	private GetItemListService getItemListService;
 
 	/**
-	 * 商品一覧を表示する.
-	 * statusがnullの時はid順に並べる。
+	 * 商品一覧を表示する. statusがnullの時はid順に並べる。
 	 * 
 	 * @param status 並び替えるパラメータ
 	 * @param model  モデル
@@ -32,13 +31,43 @@ public class ShowItemListController {
 	 */
 	@RequestMapping("")
 	public String showList(String status, Model model) {
-		
-		if(status == null) {
+
+		if (status == null) {
 			status = "id";
 		}
 		List<Item> itemList = getItemListService.getAll(status);
 		model.addAttribute("itemList", itemList);
 		model.addAttribute("status", status);
+
+		return "item_list";
+	}
+
+	/**
+	 * 商品名のあいまい検索を行う. <br>
+	 * 入力が空だった場合は全件検索を行う。
+	 * 一致するものがなければメッセージを表示する。
+	 * 
+	 * @param name 検索する名前
+	 * @param status 並び替えをするステータス
+	 * @return 商品一覧ページ
+	 */
+	@RequestMapping("/search-like-name")
+	public String searchLikeName(String code, String status, Model model) {
+		List<Item> itemList;
+		if ("".equals(code)) {
+			itemList = getItemListService.getAll(status);
+		} else {
+			itemList = getItemListService.searchLikeName(code, status);
+		}
+		
+		// 一致するものがなかった場合は商品一覧とメッセージを表示する
+		if(itemList.size() == 0) {
+			itemList = getItemListService.getAll(status);
+			model.addAttribute("notMatchMessage", "該当する商品がありません");
+		}
+		model.addAttribute("itemList", itemList);
+		model.addAttribute("status", status);
+		model.addAttribute("code", code);
 
 		return "item_list";
 	}
