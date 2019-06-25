1956ba3c5e3684e5463504de5121f7997684d253
momoyo.kanie <momoyo.kanie@rakus.co.jp>
2019-06-25 17:53:22 +0900

    ビューにタブを追加しました。

---
+++ src/main/java/com/example/ecommerce_b/repository/DrinkRepository.java
@@ -0,0 +1,14 @@
+package com.example.ecommerce_b.repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * drinksを操作するリポジトリ.
+ * 
+ * @author momoyo kanie
+ *
+ */
+@Repository
+public class DrinkRepository {
+
+}
---
+++ src/main/java/com/example/ecommerce_b/repository/SideMenuRepository.java
@@ -0,0 +1,16 @@
+package com.example.ecommerce_b.repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * sideMenusを操作するリポジトリ.
+ * 
+ * @author momoyo kanie
+ *
+ */
+@Repository
+public class SideMenuRepository {
+	
+
+
+}
